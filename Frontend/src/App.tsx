94864d435e6730fbe13dfb0213f59e1018ee3a24
ANTONIO\antoniostefanovski <antonio.stefanovski@yahoo.com>
2024-02-08 17:55:36 +0100

    Added Profile Preview Page with Responsive Design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import ProfilePreview from './views/ProfilePreview/ProfilePreview';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={ <Home/> }/>
+        <Route path="/profile" element={ <ProfilePreview/> }/>
       </Routes>
       <Footer/>
     </div>
